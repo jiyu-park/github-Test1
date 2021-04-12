15ff9eaab6abe852b0e904671c4144cc94ecc861
jiyuPark <>
2021-04-12 14:57:42 +0000

    little bit

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		int a ;
+		Scanner scan = new Scanner(System.in);
+		a = scan.nextInt();
+		System.out.printf("%d",a);
+	}
+
 }
